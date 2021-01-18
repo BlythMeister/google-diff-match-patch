5d1e7c8f5182b05b899479c426afde4412b48127
David-Tillmann Schaefer <codingdave@gmail.com>
2021-01-18 20:44:39 +0100

    adds signing keys and signs assemblies

--- src/google-diff-match-patch/Properties/AssemblyInfo.cs
+++ src/google-diff-match-patch/Properties/AssemblyInfo.cs
@@ -1,3 +1,8 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("google-diff-match-patch-tests")]
+[assembly: InternalsVisibleTo("google-diff-match-patch-tests,PublicKey=" +
+    "0024000004800000940000000602000000240000525341310004000001000100d585a6a51ac628" +
+    "edf3827b6623efeb1747d3bcb093c532b76e8aad6c7163379dbc3cad46d420b891828e02c549ad" +
+    "9a61e26904f21bea76dfed061aa833a537820f8161bf0c9ef2cc7c0ec18f75b04df69eec994ded" +
+    "1138fbe1b3382188cca9d79dfd521d72e263a28a9850edd90b7741b1e6058e5793652372e44319" +
+    "3f30bae6")]
